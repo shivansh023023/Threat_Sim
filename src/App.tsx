4774999ac3af467be330bd4dc81cd948878c2ff2
shivansh023023 <168457476+shivansh023023@users.noreply.github.com>
2025-07-27 14:33:28 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,17 @@ import { BookOpen, GamepadIcon, FlaskRound as Flask, Shield, Mailbox as Toolbox,
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
 import Journey from './pages/Journey';
+<<<<<<< HEAD
+import SimulationLab from './pages/SimulationLab';
+
+=======
+>>>>>>> aa0f564624a832de72db03b6b63f012c5d5f5e1f
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/simulation-lab" element={<SimulationLab />} />
       <Route path="/" element={<Journey />} />
     </Routes>
   );
